faedd3c6551af558e9a20da6cb0056b23e8c3bed
DenitsaBebrevenska <denitsabebrevenska@gmail.com>
2023-09-19 21:17:07 +0300

    Added exercises from the first 2 chapters and some arrays

---
+++ Arrays/CommonElements/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CommonElements
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			
+		}
+	}
+}
---
+++ Arrays/CondenseArrayToNumber/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace CondenseArrayToNumber
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] input = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+			int[] condencedNumbers = new int[input.Length - 1];
+
+			// 2 10 3 - 3 elements, 0 , 1 , 2 indexes
+			// 12 13 - 2 elements , 0 , 1 index
+			// 25 null - 1 element, 0 index
+			for (int i = ; i < length; i++)
+			{
+
+			}
+
+        }
+	}
+}

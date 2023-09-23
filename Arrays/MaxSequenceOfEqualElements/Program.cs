86a3bd7af1f9bc09015009da221f01d4b079d851
DenitsaBebrevenska <denitsabebrevenska@gmail.com>
2023-09-23 23:35:15 +0300

    Arrays more exercises added

--- Arrays/MaxSequenceOfEqualElements/Program.cs
+++ Arrays/MaxSequenceOfEqualElements/Program.cs
@@ -1,27 +1,35 @@
-﻿namespace MaxSequenceOfEqualElements
+﻿using System.Xml;
+
+namespace MaxSequenceOfEqualElements
 {
 	internal class Program
 	{
 		static void Main(string[] args)
-		{
+		{	
 			int[] numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
-			//0 1 2 3 4 5 6 7
-			//1 1 1 2 3 1 3 3
-		
-			for (int i = 0; i < numbers.Length - 1; i++) //don`t check the last number, can`t start a sequence there
-			{ //tho I dont need to loop through numbers if they are already part of a sequence
-				
-				for (int k = 1; k < numbers.Length - 1; k++)
+			int sequenceNumber = 0;
+			int counter = 1, maxCount = 1;
+	
+			for (int i = 0; i < numbers.Length-1; i++)
+			{
+				if (numbers[i] == numbers[i+1])
 				{
-					if (numbers[i] == numbers[i + k])
+					counter++;
+					if (counter > maxCount)
 					{
-
+						maxCount = counter;
+						sequenceNumber = numbers[i];				
 					}
-					else { }
-
 				}
-				
+				else
+				{
+					counter = 1;
+				}
 			}
-		}
+			for (int i = 1; i <= maxCount; i++)
+			{
+                Console.Write(sequenceNumber + " ");
+            }
+        }
 	}
 }

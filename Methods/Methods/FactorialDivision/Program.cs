4b4b09603fe3e60d42c0f166d7f08f9b64077e95
DenitsaBebrevenska <denitsabebrevenska@gmail.com>
2023-09-30 21:07:45 +0300

    added exercises in Methods

---
+++ Methods/Methods/FactorialDivision/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace FactorialDivision
+{
+	internal class Program
+	{
+		static void Main()
+		{
+			long number1 = long.Parse(Console.ReadLine());
+			long number2 = long.Parse(Console.ReadLine());
+			decimal resultDivision = Divide(number1, number2);
+			Console.WriteLine($"{resultDivision:f2}");
+		}
+
+		static long CalculateFactorial(long number)
+		{
+			long factorial = 1;
+			for (long i = 1; i <= number; i++)
+			{
+				factorial *= i;
+			}
+			return factorial;
+		}
+
+		static decimal Divide(long number1, long number2)
+		{
+			number1 = CalculateFactorial(number1);
+			number2 = CalculateFactorial(number2);
+			return (decimal)number1 / (decimal)number2;
+		}
+	}
+}

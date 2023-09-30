0d13834023c2733bb56ebdd003ee3c5051b924f1
DenitsaBebrevenska <denitsabebrevenska@gmail.com>
2023-09-30 13:02:35 +0300

    Added folder Methods and exercises from the lab

--- Arrays/LadyBugs/Program.cs
+++ Arrays/LadyBugs/Program.cs
@@ -12,7 +12,7 @@ namespace LadyBugs
 			int[] board = new int[fieldSize];
 			for (int i = 0; i < startingLocations.Length; i++) //populate the initial board with 1 and 0
 			{
-				if (startingLocations[i] >= 0 && startingLocations[i] < fieldSize) //some indexes could be outside of board so...
+				if (startingLocations[i] >= 0 && startingLocations[i] < fieldSize) //some indexes given could be outside of board
 				{ board[startingLocations[i]] = 1; }
 			}
 			while (move != "end")
@@ -22,7 +22,7 @@ namespace LadyBugs
 				int flyLenght = int.Parse(moveDetails[2]);
 				string direction = moveDetails[1];
 				int currentPosition = ladybugIndex;
-				//check initial insructions = not outside of board and if there is a bug to move there, otherwise read next move
+				//check instructions = not outside of board and if there is a bug to move there, otherwise read next move
 				if (ladybugIndex >= 0 && ladybugIndex <= fieldSize - 1 && board[ladybugIndex] == 1)
 				{
 					while (true)
@@ -54,7 +54,7 @@ namespace LadyBugs
 						}
 						else if (direction == "right")
 						{
-							if (currentPosition + flyLenght <= fieldSize - 1)
+							if (currentPosition + flyLenght <= fieldSize - 1 && currentPosition + flyLenght >= 0)
 							{
 								if (board[currentPosition + flyLenght] == 0) // moved left and  the spot is vacant
 								{
@@ -78,9 +78,7 @@ namespace LadyBugs
 							}
 						}
 					}
-
 				}
-				
 				move = Console.ReadLine();
 			}
 			Console.WriteLine(string.Join(" ", board)); 

8b7aeb89cdef803a87528708e4f9a86fbe5c35bb
DenitsaBebrevenska <denitsabebrevenska@gmail.com>
2023-10-18 16:39:25 +0300

    added attempts count and simulation of computer thinkinh

--- GuessTheNumber/GuessTheNumber/GuessTheNumber/GuessTheNumber.cs
+++ GuessTheNumber/GuessTheNumber/GuessTheNumber/GuessTheNumber.cs
@@ -1,61 +1,138 @@
-﻿namespace GuessTheNumber
+﻿using System.Threading.Channels;
+
+namespace GuessTheNumber
 {
 	internal class GuessTheNumber
 	{
+		static bool _restart = true;
 
 		static void Main(string[] args)
 		{
-			Introduction();
-			int numberToGuess = GenerateRandomNumber();
-			Console.WriteLine("Make your guess:");
-
-			while (true)
+			while (_restart)
 			{
-				string input = Console.ReadLine();
-				int playerGuess = 0;
-				try
+				Introduction();
+				int numberToGuess = GenerateRandomNumber();
+				Console.WriteLine("Make your guess:");
+				int playerAttempts = 0;
+				while (true)
 				{
-					playerGuess = int.Parse(input);
-				}
-				catch (Exception)
-				{
-                    Console.WriteLine("You input is invalid!");
-                    Console.WriteLine("Enter a number between 1 and 100 inclive:");
-					continue;
-                }
+					int playerGuess = CheckPlayerInput();
 
-				if (playerGuess == numberToGuess)
-				{
-					Console.WriteLine("Congratulations! You won!");
-					break;
+					if (playerGuess == numberToGuess)
+					{
+						Console.WriteLine("Congratulations!");
+						Console.WriteLine("YOU WIN!");
+						break;
+					}
+					else if (playerGuess > numberToGuess)
+					{
+						Console.WriteLine(playerGuess - numberToGuess <= 5
+							? "High, but you are really close!"
+							: "Too high! Make another guess: ");
+						playerAttempts = IncrementAttempts(playerAttempts);
+					}
+					else if (playerGuess < numberToGuess)
+					{
+						Console.WriteLine(numberToGuess - playerGuess <= 5
+							? "Low, but you are really close!"
+							: "Too low! Make another guess: ");
+						playerAttempts = IncrementAttempts(playerAttempts);
+					}
+
+					if (AttemptsOver(playerAttempts))
+					{
+						Console.WriteLine("You ran out of guess attempts!");
+						Console.WriteLine("YOU LOSE!");
+						break;
+					}
 				}
-				else if (playerGuess > numberToGuess)
-				{
-					Console.WriteLine("Too high! Make another guess: ");
-				}
-				else if (playerGuess < numberToGuess)
-				{
-                    Console.WriteLine("Too low! Make another guess: ");
-                }
+				OfferRestartOrEnd();
 			}
 		}
+
 		static void Introduction()
 		{
 			Console.WriteLine("Let`s play a game of Guess The number!");
 			Console.WriteLine("The computer is picking a number between 1 and 100 inclusive...");
-			//add timer
-			
+			SimulateThinking();
 			Console.WriteLine("Time to guess!");
-			Console.WriteLine("Beware though you have 30 seconds to find the answer or you`ll lose!"); //timer or attempts?
-            Console.WriteLine("Press any key to start the game: ");
+			Console.WriteLine("Beware! You have 10 attempts to find the answer or you`ll lose!");
+			Console.WriteLine("Press any key to start the game: ");
 			Console.ReadKey();
-            Console.Clear();
+			Console.Clear();
 		}
 		static int GenerateRandomNumber()
 		{
 			Random randomNumber = new Random();
 			int numberToGuess = randomNumber.Next(1, 101);
 			return numberToGuess;
+		}
+		static void SimulateThinking()
+		{
+			for (int i = 0; i < 3; i++)
+			{
+				Console.Write(". ");
+				System.Threading.Thread.Sleep(600);
+			}
+
+			Console.WriteLine();
+		}
+
+		static void OfferRestartOrEnd()
+		{
+			Console.WriteLine("You can restart the game by pressing any key!");
+			Console.WriteLine("Or press ESC to exit the game!");
+			Console.WriteLine("Restart or exit ?");
+			var keyPressed = Console.ReadKey();
+			if (keyPressed.KeyChar == 27)
+			{
+				_restart = false;
+			}
+			else
+			{
+				Console.Clear();
+			}
+		}
+
+		static int CheckPlayerInput()
+		{
+			while (true)
+			{
+				string input = Console.ReadLine();
+				int playerGuess;
+
+				try
+				{
+					playerGuess = int.Parse(input);
+				}
+				catch (Exception)
+				{
+					Console.WriteLine("Your input is invalid!");
+					Console.WriteLine("Enter a number between 1 and 100 inclusive:");
+					continue;
+				}
+
+				if (playerGuess  is <= 0 or > 100)
+				{
+					Console.WriteLine("Invalid number!");
+					Console.WriteLine("Enter a number between 1 and 100 inclusive:");
+					continue;
+				}
+
+				return playerGuess;
+			}
+		}
+
+		static int IncrementAttempts(int attempts)
+		{
+			Console.WriteLine($"Attempts to guess: {++attempts}/ 10!");
+			return attempts;
+
+		}
+
+		static bool AttemptsOver(int attempts)
+		{
+			return attempts >= 10;
 		}
 	}
 }

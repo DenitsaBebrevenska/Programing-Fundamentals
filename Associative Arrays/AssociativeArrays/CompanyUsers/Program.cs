044cd7fa731f803fd7d16b4c209ccd3df3fb661a
DenitsaBebrevenska <denitsabebrevenska@gmail.com>
2023-10-26 20:30:47 +0300

    Objects and classes added exercises

--- Associative Arrays/AssociativeArrays/CompanyUsers/Program.cs
+++ Associative Arrays/AssociativeArrays/CompanyUsers/Program.cs
@@ -2,9 +2,39 @@
 {
 	internal class Program
 	{
-		static void Main(string[] args)
+		static void Main()
 		{
-			Console.WriteLine("Hello, World!");
+			string input;
+			Dictionary<string, List<string>> usersDictionary = new Dictionary<string, List<string>>();
+
+			while ((input = Console.ReadLine()) != "End")
+			{
+				string[] userDetails = input.Split(" -> ");
+				string companyName = userDetails[0];
+				string userId = userDetails[1];
+
+				if (!usersDictionary.ContainsKey(companyName))
+				{
+					usersDictionary.Add(companyName, new List<string>());
+				}
+
+				List<string> currentCompany = usersDictionary[companyName];
+
+				if (currentCompany.Contains(userId))
+				{
+					continue;
+				}
+				currentCompany.Add(userId);
+			}
+
+			foreach (var kvp in usersDictionary)
+			{
+				Console.WriteLine(kvp.Key);
+				foreach (var id in kvp.Value)
+				{
+					Console.WriteLine($"-- {id}");
+				}
+			}
 		}
 	}
 }

055243232a00d9050544b316991375ae25d61b6e
DenitsaBebrevenska <denitsabebrevenska@gmail.com>
2023-11-03 19:35:59 +0200

    added exercise to Data types and variables

---
+++ RegularExpressions/RegularExpressions/Race/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+
+namespace Race
+{
+	internal class Program
+	{
+		static void Main()
+		{
+			string[] participants = Console.ReadLine().Split(", ");
+			string input;
+
+			while ((input = Console.ReadLine()) != "end of the race")
+			{
+				
+			}
+		}
+	}
+}

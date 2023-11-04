37dbaa3d66787d1f1ad0caebe043b402bed4b506
DenitsaBebrevenska <denitsabebrevenska@gmail.com>
2023-11-04 23:40:21 +0200

    added exercise to Data types

---
+++ DataTypesAndVariables/StringConcatenation/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace StringConcatenation
+{
+	internal class Program
+	{
+		static void Main()
+		{
+			char delimiter = Console.ReadLine()[0];
+			string type = Console.ReadLine();
+			sbyte lines = sbyte.Parse(Console.ReadLine());
+
+			for (int i = 0; i < lines; i++)
+			{
+				if (type == "even")
+				{
+
+				}
+			}
+		}
+	}
+}

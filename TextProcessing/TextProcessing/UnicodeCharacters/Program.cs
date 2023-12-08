eb3e5d30b02b2a9cb7858ac10c0eca69240cb98a
DenitsaBebrevenska <denitsabebrevenska@gmail.com>
2023-12-09 01:05:13 +0200

    Unicode Characters added

---
+++ TextProcessing/TextProcessing/UnicodeCharacters/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace UnicodeCharacters
+{
+	internal class Program
+	{
+		static void Main()
+		{
+			string text = Console.ReadLine();
+			List<string> convertedStrings = new List<string>();
+
+			for (int i = 0; i < text.Length; i++)
+			{
+				convertedStrings.Add(@$"\u{(int)text[i]:x4}");
+			}
+
+			Console.WriteLine(string.Join("",convertedStrings));
+		}
+	}
+}

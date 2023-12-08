cce8fb127237546acb408ce6aa7d341eb9922a1e
DenitsaBebrevenska <denitsabebrevenska@gmail.com>
2023-12-09 01:06:58 +0200

    Unicode Characters  removed unnecessary list usage

--- TextProcessing/TextProcessing/UnicodeCharacters/Program.cs
+++ TextProcessing/TextProcessing/UnicodeCharacters/Program.cs
@@ -5,14 +5,11 @@
 		static void Main()
 		{
 			string text = Console.ReadLine();
-			List<string> convertedStrings = new List<string>();
 
 			for (int i = 0; i < text.Length; i++)
 			{
-				convertedStrings.Add(@$"\u{(int)text[i]:x4}");
+				Console.Write($"\\u{(int)text[i]:x4}");
 			}
-
-			Console.WriteLine(string.Join("",convertedStrings));
 		}
 	}
 }

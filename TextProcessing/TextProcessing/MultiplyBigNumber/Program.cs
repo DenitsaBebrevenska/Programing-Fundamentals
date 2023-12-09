24a68bccd1f1d83794352eb68b80d8e634728592
DenitsaBebrevenska <denitsabebrevenska@gmail.com>
2023-12-10 00:57:52 +0200

    removed redundant int type cast

--- TextProcessing/TextProcessing/MultiplyBigNumber/Program.cs
+++ TextProcessing/TextProcessing/MultiplyBigNumber/Program.cs
@@ -8,7 +8,7 @@ namespace MultiplyBigNumber
 		{
 			//BigInteger is not supposed to be used in this assignment
 
-			string bigNumber = Console.ReadLine();
+			string bigNumber = Console.ReadLine().TrimStart('0');
 			int singleDigit = int.Parse(Console.ReadLine());
 
 			if (singleDigit == 0) //avoiding making a string full of 0s
@@ -21,7 +21,7 @@ namespace MultiplyBigNumber
 			int leftOver = 0;
 			for (int i = bigNumber.Length - 1; i >= 0; i--)
 			{
-				int currentNumber = (int)bigNumber[i] - '0';
+				int currentNumber = bigNumber[i] - '0';
 				int result = currentNumber * singleDigit + leftOver;
 				int numberToAppend = result % 10;
 				sb.Append(numberToAppend);
